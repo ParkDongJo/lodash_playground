b779429a195df3234e2d373ad8b7cc1c8f3d5c59
ParkDongJo <pdj10211@gmail.com>
2024-09-30 00:01:21 +0900

    함수조합의 각 함수 분리

--- ts/pipeline_2.ts
+++ ts/pipeline_2.ts
@@ -4,11 +4,15 @@ const ids = [1, 2, 3, 4];
 const names = ['carA', 'carB', 'carC', 'carD'];
 const likes = [20, 30, 10, 22];
 
+const sortByLast = _.partial(_.sortBy as any, _, [_.last]);
+
+const mapByNth = (index: number) => _.partial(_.map as any, _, _.partial(_.nth, _, index))
+
 const popularCarName = _.flow([
     _.zip,
-    _.partial(_.sortBy as any, _, [_.last]),
+    sortByLast,
     _.reverse,
-    _.partial(_.map as any, _, _.partial(_.nth, _, 1)),
+    mapByNth(1),
     _.head,
 ])
 
